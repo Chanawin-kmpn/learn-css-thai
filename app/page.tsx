a862e5e71f0338c9cf1a2892266fd2759c5d5b43
Pond <pchanawin@gmail.com>
2024-12-19 15:37:44 +0700

    update: update logo, font and tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <h1 className="text-3xl font-black text-white">Welcome to Nextjs</h1>
+      <h1 className="">ยินดีต้อนรับ</h1>
+      <p className="text-center">
+        นี่คือเว็บไซต์ที่สร้างด้วย Next.js และ Tailwind CSS
+      </p>
+      this is
+      <p className="font-inter">Welcome to css</p>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,10 +10,28 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark: {
+          "700": "#3f3f46",
+          "900": "#18181b",
+        },
+        light: {
+          "300": "#d4d4d8",
+          "400": "#a1a1aa",
+        },
+      },
+      backgroundImage: {
+        "light-desktop":
+          "url(/images/background/light-blur-blob-bg-desktop.png)",
+        "dark-desktop": "url(/images/background/dark-blur-blob-bg-desktop.png)",
+        "light-mobile": "url(/images/background/dark-blur-blob-bg-mobile.png)",
+        "dark-mobile": "url(/images/background/dark-blur-blob-bg-mobile.png)",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        sarabun: ["var(--font-sarabun)"],
       },
     },
   },
-  plugins: [],
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
